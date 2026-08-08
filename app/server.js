2ea0f9bb1c91082d651cd611f2e634e78a76d23c
Vanshika Sharma <306284690+vanshikasharma33@users.noreply.github.com>
2026-08-08 18:23:46 +0530

    Fix Node server binding for Docker

--- app/server.js
+++ app/server.js
@@ -13,6 +13,6 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Server running on port 3000");
 });
